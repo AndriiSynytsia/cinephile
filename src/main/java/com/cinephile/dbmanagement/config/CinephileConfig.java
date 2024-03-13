2d9f60c95b19393b33abefaea0cc2d85b1d4e2ef
Andrii Synytsia <andreysinitsa4315@gmail.com>
2024-03-13 16:42:27 -0600

    Added viewResolver for appearing the movie-list page

--- src/main/java/com/cinephile/dbmanagement/config/CinephileConfig.java
+++ src/main/java/com/cinephile/dbmanagement/config/CinephileConfig.java
@@ -1,11 +1,20 @@
 package com.cinephile.dbmanagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.cinephile"})
+@ComponentScan(basePackages = { "com.cinephile" })
 public class CinephileConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
--- src/main/java/com/cinephile/dbmanagement/config/CinephileInitializer.java
+++ src/main/java/com/cinephile/dbmanagement/config/CinephileInitializer.java
@@ -4,7 +4,6 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class CinephileInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
     @Override
     protected Class<?>[] getRootConfigClasses() {
-//        return new Class[0];
         return null;
     }
 
